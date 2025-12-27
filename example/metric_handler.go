07b16e5ccb5332067a1ef54bd21c35557293e4b0
CreamSodass <greatvaluecreamsoda@gmail.com>
2025-12-27 19:54:30 +0000

    Remove Crappy CLI Example

--- example/metric_handler.go
+++
@@ -1,8 +0,0 @@
-package main
-
-// MetricHandler is the interface that every metric must implement
-type MetricHandler interface {
-	Name() string
-	Close()
-	Compute(a, b *frame) (map[string]float64, error)
-}
